a58e0ebf09ef252217639e214a5075e8ceece4d0
agius <andrew@atevans.com>
2014-02-24 09:05:04 -0800

    exists js function - equivalent to rails obj.present

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,38 @@ function get_timestamp(){
   var d = new Date();
   return d.getTime().toString();
 }
+
+/*
+  returns true unless:
+    empty string
+    empty object
+    NaN
+    null
+    false
+    undefined
+    empty array
+*/
+
+function exists(arg){
+  if(typeof(arg) === 'undefined'){
+    return false;
+  } else if(arg === false){
+    return false
+  } else if(typeof(arg) === 'string'){
+    return arg.trim().length > 0;
+  } else if(typeof(arg) === 'number'){
+    return !isNaN(arg);
+  } else if(typeof(arg) === "object"){
+    if(arg === null){
+      return false;
+    } else if(typeof(arg.length) !== 'undefined'){
+      return arg.length > 0
+    } else {
+      // if it has any enumerable properties, return true
+      for (var x in arg){ 
+        return true;
+      }
+    }
+  }
+  return true;
+}
